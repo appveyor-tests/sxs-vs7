3ca807f21bb842bfb8f8b11c86c7b5cc60345ef4
Alena Hrynkevich <alena@appveyor.com>
2019-08-20 17:18:24 -0700

    Create build.ps1

---
+++ build.ps1
@@ -0,0 +1,4 @@
+    $vs12_path = ((Get-ItemProperty 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7' -Name '12.0') | Select-Object -ExpandProperty '12.0')
+    if(-not $vs12_path) {
+        throw 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7 key does not contain 12.0 value'
+    }
